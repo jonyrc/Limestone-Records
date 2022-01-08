4250e3aab4f806b4da169a830b2a1350250d30be
Jonathan Carvalho <jonyrc@hotmail.com.br>
2022-01-08 12:45:23 -0300

    Creating home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <div class="text-top">Bem vindo ao Limestone Records</div>
+
+    <a href="/" target="_blank">
+      <img alt="Limestone Records" id="logo" src="../assets/logo.png" />
+    </a>
+
+    <div class="text-bottom">
+      Selo do movimento Limestone, de Arcos-MG e região
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "home-page",
+};
+</script>
+
+<style scoped>
+main {
+  background-color: var(--color-background-home);
+  justify-content: center;
+  align-items: center;
+}
+
+.text-top {
+  color: var(--color-text-dark);
+  font-size: 22px;
+  margin-bottom: 22px;
+}
+
+.text-bottom {
+  color: var(--color-text-dark);
+  font-size: 18px;
+  margin-top: 22px;
+}
+
+#logo {
+  width: 300px;
+}
+
+@media (min-width: 700px) {
+  #logo {
+    width: 500px;
+  }
+}
+</style>
