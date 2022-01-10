1e2b49b211594677e464415668d09b7ace0396c6
Jonathan Carvalho <jonyrc@hotmail.com.br>
2022-01-10 19:28:10 -0300

    Footer created

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header />
     <Home />
+    <Footer />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 import "./styles/global.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
   components: {
     Header,
     Home,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer>
+    <a
+      class="socialLink"
+      target="_blank"
+      href="https://www.facebook.com/limestonerecords"
+    >
+      <img src="@/assets/icons/facebook.svg" title="Facebook" alt="Facebook" />
+    </a>
+    <a
+      class="socialLink"
+      target="_blank"
+      href="https://www.instagram.com/limestonerecords/"
+    >
+      <img src="@/assets/icons/instagram.svg" title="" alt="" />
+    </a>
+    <a
+      class="socialLink"
+      target="_blank"
+      href="https://open.spotify.com/user/hgjxhdsfk36tx9ckxs8zp8amg?si=838c9569b369478a"
+    >
+      <img src="@/assets/icons/spotify.svg" title="" alt="" />
+    </a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footer-main",
+};
+</script>
+
+<style scoped>
+footer {
+  height: 80px;
+  width: 100%;
+  background-color: var(--color-background-nav);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.socialLink {
+  height: 40px;
+  display: flex;
+  margin: 20px;
+}
+</style>
